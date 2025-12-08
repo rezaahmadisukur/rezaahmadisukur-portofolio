60afc33e9c3292f4509a1a461b5fc2809e0ca50e
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-12-08 08:45:17 +0700

    fix: remove state never use and not type in state type

--- src/components/animate-ui/components/animate/tabs.tsx
+++ src/components/animate-ui/components/animate/tabs.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 import {
   Tabs as TabsPrimitive,
   TabsList as TabsListPrimitive,
@@ -12,16 +10,16 @@ import {
   type TabsListProps as TabsListPrimitiveProps,
   type TabsTriggerProps as TabsTriggerPrimitiveProps,
   type TabsContentProps as TabsContentPrimitiveProps,
-  type TabsContentsProps as TabsContentsPrimitiveProps,
-} from '@/components/animate-ui/primitives/animate/tabs';
-import { cn } from '@/lib/utils';
+  type TabsContentsProps as TabsContentsPrimitiveProps
+} from "@/components/animate-ui/primitives/animate/tabs";
+import { cn } from "@/lib/utils";
 
 type TabsProps = TabsPrimitiveProps;
 
 function Tabs({ className, ...props }: TabsProps) {
   return (
     <TabsPrimitive
-      className={cn('flex flex-col gap-2', className)}
+      className={cn("flex flex-col gap-2", className)}
       {...props}
     />
   );
@@ -34,8 +32,8 @@ function TabsList({ className, ...props }: TabsListProps) {
     <TabsHighlightPrimitive className="absolute z-0 inset-0 border border-transparent rounded-md bg-background dark:border-input dark:bg-input/30 shadow-sm">
       <TabsListPrimitive
         className={cn(
-          'bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]',
-          className,
+          "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+          className
         )}
         {...props}
       />
@@ -51,7 +49,7 @@ function TabsTrigger({ className, ...props }: TabsTriggerProps) {
       <TabsTriggerPrimitive
         className={cn(
           "data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md w-full px-2 py-1 text-sm font-medium whitespace-nowrap transition-colors duration-500 ease-in-out focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-          className,
+          className
         )}
         {...props}
       />
@@ -70,7 +68,7 @@ type TabsContentProps = TabsContentPrimitiveProps;
 function TabsContent({ className, ...props }: TabsContentProps) {
   return (
     <TabsContentPrimitive
-      className={cn('outline-none', className)}
+      className={cn("outline-none", className)}
       {...props}
     />
   );
@@ -86,5 +84,5 @@ export {
   type TabsListProps,
   type TabsTriggerProps,
   type TabsContentsProps,
-  type TabsContentProps,
+  type TabsContentProps
 };
